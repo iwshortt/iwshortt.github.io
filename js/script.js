e43300e30c80779d08a0297e9daf6bda6e7e93ba
mordecai-x <105885497+mordecai-x@users.noreply.github.com>
2023-09-12 19:41:06 -0400

    rough draft
    intitial basis for the portfolio

---
+++ js/script.js
@@ -0,0 +1,43 @@
+// ------------------------------------------------------- //
+// ----------------------- Base -------------------------- //
+// ------------------------------------------------------- //
+const primaryNav = document.querySelector('.primary-navigation');
+const navToggle = document.querySelector('.mobile-nav-toggle');
+
+navToggle.addEventListener('click', () => {
+    const visibility = primaryNav.getAttribute('data-visible');
+    if (visibility === "false") {
+        primaryNav.setAttribute('data-visible', true);
+        navToggle.setAttribute('aria-expanded', true);
+    } else if (visibility === "true") {
+        primaryNav.setAttribute('data-visible', false);
+        navToggle.setAttribute('aria-expanded', false);
+    }
+});
+
+// ------------------------------------------------------- //
+// ----------------------- Base -------------------------- //
+// ------------------------------------------------------- //
+const projectTop = document.querySelector('.project-top');
+
+document.addEventListener('mousemove', (e) => {
+    rotateElement(e, projectTop);
+});
+
+function rotateElement(event, element) {
+    // get mouse position
+    const x = event.clientX;
+    const y = event.clientY;
+
+    // find the middle of project ** fix to find the middle of the project-top div **
+    const middleX = window.innerWidth / 2;
+    const middleY = window.innerHeight / 2;
+
+    // get offset from middle
+    const offsetX = ((x - middleX) / middleX) * 3;
+    const offsetY = ((y - middleY) / middleY) * 3;
+    console.log(offsetX, offsetY);
+
+    element.style.setProperty('--rotateX', offsetY + 'deg');
+    element.style.setProperty('--rotateY', -1 * offsetX + 'deg');
+}
