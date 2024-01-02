659044c7ec3ad737e2b4eab35823570f8f5e821b
mordecai-x <105885497+mordecai-x@users.noreply.github.com>
2024-01-02 17:48:16 -0500

    mobile viewport fix
    works but need to call the function first outside the advent listener so that the page loads with the correct size
    -- attempt to place the height on the body instead of each individual element

--- js/script.js
+++ js/script.js
@@ -45,7 +45,8 @@ navToggle.addEventListener('click', () => {
 // ------------------------------------------------------- //
 // --------------- viewport fix for mobile --------------- //
 // ------------------------------------------------------- //
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
 window.addEventListener('resize', () => {
-    let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
 });
