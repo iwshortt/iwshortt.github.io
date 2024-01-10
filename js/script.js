caead0cab0c2a1730df7f118521b02e4a17bc711
mordecai-x <105885497+mordecai-x@users.noreply.github.com>
2024-01-10 00:16:53 -0500

    exceeds expectations (2nd draft)
    deleted css and started from scratch
      - site was not not responsive
      - added svgs to contact icons
      - restructured mobile, tablet, and larger displays
      - added self portrait
      - updated 'logo' in nav
    added new function to scripts
      - added 'light/dark' mode feature

--- js/script.js
+++ js/script.js
@@ -3,8 +3,9 @@
 // ------------------------------------------------------- //
 const primaryNav = document.querySelector('.primary-navigation');
 const navToggle = document.querySelector('.mobile-nav-toggle');
+const nav = document.querySelector('nav');
 
-navToggle.addEventListener('click', () => {
+navToggle.addEventListener('click', () => { // created new eventListener due to 'bubbling' and 'scope'
     const visibility = primaryNav.getAttribute('data-visible');
     if (visibility === "false") {
         primaryNav.setAttribute('data-visible', true);
@@ -14,11 +15,52 @@ navToggle.addEventListener('click', () => {
         navToggle.setAttribute('aria-expanded', false);
     }
 });
+nav.addEventListener('click', (e) => { // closes nav overlay when 'a' tag is clicked
+    const navLinks = document.querySelectorAll('nav a');
+    for (let i = 0; i < navLinks.length; i++) {
+        if (e.target === navLinks[i] || e.target === navToggle) {
+            primaryNav.setAttribute('data-visible', false);
+            navToggle.setAttribute('aria-expanded', false);
+        }
+    }
+});
+
+// ------------------------------------------------------- //
+// --------------- light/dark mode toggle ---------------- //
+// ------------------------------------------------------- //
+let darkMode = localStorage.getItem('darkMode');
+const darkModeToggle = document.querySelector('#dark-mode-toggle');
+
+const enableDarkMode = () => {
+    // 1. add class darkmode to the body
+    document.body.classList.add('dark-mode');
+    // 2 update darkmode in the localStorage
+    localStorage.setItem('darkMode', 'enabled');
+}
+
+const disableDarkMode = () => {
+    // 1. remove class darkmode from the body
+    document.body.classList.remove('dark-mode');
+    // 2 update darkmode in the localStorage
+    localStorage.setItem('darkMode', null);
+}
+
+if (darkMode === 'enabled') {
+    enableDarkMode();
+}
+
+darkModeToggle.addEventListener('click', () => {
+    darkMode = localStorage.getItem('darkMode');
+    if (darkMode !== 'enabled') {
+        enableDarkMode();
+    } else {
+        disableDarkMode();
+    }
+});
 
 // ------------------------------------------------------- //
 // --------------- viewport fix for mobile --------------- //
 // ------------------------------------------------------- //
-
 window.addEventListener('resize', () => {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
